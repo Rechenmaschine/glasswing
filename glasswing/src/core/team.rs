fc4018cad2a1d6b36a2953582620baa42c0c759a
Louis Schell <louis.schell1@gmx.de>
2024-01-07 22:25:38 +0100

    Remove concept of team polarity and organise team

--- glasswing/src/core/team.rs
+++ glasswing/src/core/team.rs
@@ -1,26 +1,18 @@
+use std::fmt;
 use crate::core::Game;
-use std::fmt::Debug;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Polarity {
-    Positive = 1,
-    Negative = -1,
-}
+pub trait GwTeam<G>: Sized + Clone + Eq + PartialEq + fmt::Debug
+    where
+        G: Game<Team=Self>,
+{
+    fn opponent(&self) -> Self;
 
-impl Polarity {
-    #[inline(always)]
-    pub fn flip(&self) -> Self {
-        match self {
-            Polarity::Positive => Polarity::Negative,
-            Polarity::Negative => Polarity::Positive,
-        }
-    }
-
-    #[inline(always)]
-    pub fn sign(&self) -> i32 {
-        match self {
-            Polarity::Positive => 1,
-            Polarity::Negative => -1,
+    #[inline]
+    fn nth(&self, n: usize) -> Self {
+        if n % 2 == 0 {
+            self.clone()
+        } else {
+            self.opponent()
         }
     }
 }
@@ -35,25 +27,6 @@ impl Team {
     }
 }
 
-pub trait GwTeam<G>
-where
-    Self: Sized + Clone + Debug + Eq + PartialEq,
-    G: Game<Team = Self>,
-{
-    fn next(&self) -> Self;
-
-    fn polarity(&self) -> Polarity;
-
-    #[inline]
-    fn nth(&self, n: usize) -> Self {
-        let mut team = self.clone();
-        for _ in 0..n {
-            team = team.next();
-        }
-        team
-    }
-}
-
 /// Team for a two player game.
 /// Why is this not an enum? Performance.
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
@@ -62,26 +35,18 @@ pub enum Team {
     Two,
 }
 
-impl<G: Game<Team = Self>> GwTeam<G> for Team {
-    #[inline(always)]
-    fn next(&self) -> Self {
+impl<G: Game<Team=Self>> GwTeam<G> for Team {
+    #[inline]
+    fn opponent(&self) -> Self {
         match self {
             Team::One => Team::Two,
             Team::Two => Team::One,
         }
     }
-
-    #[inline(always)]
-    fn polarity(&self) -> Polarity {
-        match self {
-            Team::One => Polarity::Positive,
-            Team::Two => Polarity::Negative,
-        }
-    }
 }
 
-impl std::fmt::Display for Team {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Display for Team {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Team::One => {
                 write!(f, "One")
@@ -93,8 +58,8 @@ impl std::fmt::Display for Team {
     }
 }
 
-impl Debug for Team {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Debug for Team {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Team::One => {
                 write!(f, "Team(One)")
